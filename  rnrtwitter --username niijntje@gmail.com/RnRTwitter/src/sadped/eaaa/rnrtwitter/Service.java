f96663dd51d6da11e4946117a0a8567b70b70f24
niijntje <niijntje@gmail.com>
2012-04-25 22:42:29 +0000

    De redigerbare felter i profile.xhtml har nu hver deres egen save- og cancel-knap. Mangler at undersøge hvordan de gøres ikke-redigerbare, når der er tale om andres profile, men tror ikke det er så svært.
    Har tilføjet en del metoder i Service og UserBean for at sikre, at man ikke kommer til at beholde attributter fra en tidligere currentUser, når man (forsøger at) logge ind med et nyt brugernavn. Læg især mærke til verifyUser(), der nu sørger for at oprette en ny currentUser - hvis login fejler, fås en ny med kun username beholdt - hvis login lykkes, hentes en ny delvis kopi af den 'rigtige' user fra service.
    
    Jeg har kigget meget kortvarigt på mulighed for fil-upload (tænker på profilbillede), så dét dukker måske op her og der i koden uden at være gjort funktionelt.

---  rnrtwitter --username niijntje@gmail.com/RnRTwitter/src/sadped/eaaa/rnrtwitter/Service.java
+++  rnrtwitter --username niijntje@gmail.com/RnRTwitter/src/sadped/eaaa/rnrtwitter/Service.java
@@ -18,8 +18,8 @@ public class Service implements Serializable{
 		registeredUsers.add(new User("Rita", "pw", ""));
 	}
 	
-	public void createUser(User u){
-		User newUser = new User(u.getUserName(),u.getPassword(), u.getProfileText());
+	public void createUser(User currentUser){
+		User newUser = new User(currentUser.getUserName(),currentUser.getPassword(), currentUser.getProfileText());
 		addUser(newUser);
 		System.out.println("New user:"+newUser.getUserName()+". "+newUser.getProfileText());
 		System.out.println("Updated user list: "+registeredUsers);
@@ -56,6 +56,24 @@ public class Service implements Serializable{
 		}
 		else return false;
 	}
+	
+	/**
+	 * Finder en user ud fra username og password og returnerer en kopi,
+	 * hvor alle de andre felter er korrekte eller sat til null.
+	 * Benyttes af userBean når en ny bruger logger ind, så gamle user-attributter
+	 * fra den foregående ikke 'bliver hængende' i currentUser.
+	 * 
+	 * @param currentUser
+	 * @return
+	 */
+	public User getCleanCopy(User currentUser){
+		User realUser = findUser(currentUser);
+		User u = new User(currentUser.getUserName(), currentUser.getPassword(),realUser.getProfileText());
+		u.setRealName(realUser.getRealName());
+		u.setSubscriptions(null);
+		u.setTweets(null);
+		return u;
+	}
 
 	public boolean userNameAvailable(String value) {
 		boolean available = true;
@@ -67,11 +85,19 @@ public class Service implements Serializable{
 		return available;
 	}
 
+	/**
+	 * Måske unødigt overhead at service skal søge efter samme user to gange, men
+	 * det gør det nemmere at genbruge koden, og fastholder den skarpe adskillelse
+	 * mellem userBean og 'rigtige' User-objekter.
+	 */
 	public void saveProfileText(User currentUser) {
 		User u = findUser(currentUser);
 		u.setProfileText(currentUser.getProfileText());
 	}
-	
+	public void saveRealName(User currentUser) {
+		User u = findUser(currentUser);
+		u.setRealName(currentUser.getRealName());
+	}	
 	/**
 	 * Til test af kode (især om ændringer er slået igennem helt ned i service)
 	 * Det er helt ok at ændre på hvad der bliver udskrevet! :-)
@@ -81,5 +107,6 @@ public class Service implements Serializable{
 			System.out.println(u.getUserName()+": "+u.getProfileText());
 		}
 	}
+
 
 }
